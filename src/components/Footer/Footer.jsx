3531ca3fdc29f7cc38eabf9230914ad0c2ee388d
Sldvlld <sladvlad@gmail.com>
2023-08-14 22:02:01 +0300

    add

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
+import { NavLink } from 'react-router-dom';
 import css from './Footer.module.css';
 
 const Footer = () => {
   return (
     <footer>
-      <div className={css.footerWraper}></div>
+      <div className={css.footerWraper}>
+        <NavLink className={css.footerLabel} to="/">
+          Regulbe
+        </NavLink>
+      </div>
     </footer>
   );
 };
