09765653b821372a97c503854bca1127a3a8f092
Sldvlld <sladvlad@gmail.com>
2023-08-15 12:07:53 +0300

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import css from './Footer.module.css';
+import sprite from '../../assets/sprite.svg';
 
 const Footer = () => {
   return (
@@ -8,6 +9,32 @@ const Footer = () => {
         <NavLink className={css.footerLabel} to="/">
           Regulbe
         </NavLink>
+        <div className={css.footerBlocks}>
+          <nav className={css.footerNav}>
+            <ul className={css.navList}>
+              <li className={css.navItem}>
+                <p>Terms & Conditions</p>
+              </li>
+              <li className={css.navItem}>
+                <p>Privacy Policy</p>
+              </li>
+              <li className={css.navItem}>
+                <p>Careers</p>
+              </li>
+            </ul>
+          </nav>
+          <p className={css.footerCopyright}> © Regulbe - Copyright 2023</p>
+          <div className={css.svgBlock}>
+            <NavLink to="https://www.linkedin.com/company/regulbe-ka/">
+              <svg className={css.footerSvg}>
+                <use href={sprite + '#linkedin'} />
+              </svg>
+            </NavLink>
+            {/* <svg className={css.footerSvg}>
+              <use href={sprite + '#fb'} />
+            </svg> */}
+          </div>
+        </div>
       </div>
     </footer>
   );
