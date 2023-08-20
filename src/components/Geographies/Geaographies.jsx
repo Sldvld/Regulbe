27b589e2e4dc08407c662d58a46b2255be370375
Sldvlld <sladvlad@gmail.com>
2023-08-20 18:35:50 +0300

    change routes

--- src/components/Geographies/Geaographies.jsx
+++ src/components/Geographies/Geaographies.jsx
@@ -1,22 +1,71 @@
 import css from './Geographies.module.css';
 import Map from '../../assets/map.svg';
 
-const Geographies = () => {
+// const Geographies = () => {
+//   return (
+//     <div className={css.geographiesSection} id="geographies">
+//       <div className={css.geographiesWrapper}>
+//         <div className={css.geographiesTextBlock}>
+//           <h2 className={css.geographiesHeader}>GEOGRAPHIES</h2>
+//           <p className={css.geographiesLocation}>Asia - Pacific (11)</p>
+//           <p className={css.geographiesLocation}>Eastern Europe (9)</p>
+//           <p className={css.geographiesLocation}>Western Europe (18)</p>
+//           <p className={css.geographiesLocation}>Latin America (7)</p>
+//           <p className={css.geographiesLocation}>North America (2)</p>
+//         </div>
+//         <img className={css.geographiesMap} src={Map} alt="" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Geographies;
+
+import { Accordion, AccordionItem } from '@szhsin/react-accordion';
+
+const Example = () => {
   return (
     <div className={css.geographiesSection} id="geographies">
-      <div className={css.geographiesWrapper}>
-        <div className={css.geographiesTextBlock}>
-          <h2 className={css.geographiesHeader}>GEOGRAPHIES</h2>
-          <p className={css.geographiesLocation}>Asia - Pacific (11)</p>
-          <p className={css.geographiesLocation}>Eastern Europe (9)</p>
-          <p className={css.geographiesLocation}>Western Europe (18)</p>
-          <p className={css.geographiesLocation}>Latin America (7)</p>
-          <p className={css.geographiesLocation}>North America (2)</p>
+      <div className={css.geographiesTextBlock}>
+        <div className={css.accordion}>
+          <Accordion allowMultiple>
+            <AccordionItem
+              header={
+                <div>
+                  <p className={css.licationItem}>Asia - Pacific (11)</p>
+                </div>
+              }
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </AccordionItem>
+
+            <AccordionItem header="Eastern Europe (9)">
+              Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla vel
+              erat quis sodales. Nam ex enim, eleifend venenatis lectus vitae,
+              accumsan auctor mi.
+            </AccordionItem>
+
+            <AccordionItem header="Western Europe (18)">
+              Suspendisse massa risus, pretium id interdum in, dictum sit amet
+              ante. Fusce vulputate purus sed tempus feugiat.
+            </AccordionItem>
+
+            <AccordionItem header="Latin America (7)">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </AccordionItem>
+
+            <AccordionItem header="North America (2)">
+              Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla vel
+              erat quis sodales. Nam ex enim, eleifend venenatis lectus vitae,
+              accumsan auctor mi.
+            </AccordionItem>
+          </Accordion>
         </div>
-        <img className={css.geographiesMap} src={Map} alt="" />
       </div>
     </div>
   );
 };
 
-export default Geographies;
+export default Example;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,9 +4,10 @@ import AboutUs from 'components/AboutUs/AboutUs';
 import Services from 'components/Services/Services';
 import Expertise from 'components/Experetise/Expertise';
 import Questions from 'components/Questions/Questions';
-import Geographies from 'components/Geographies/Geaographies';
+// import Geographies from 'components/Geographies/Geaographies';
 import Pricing from 'components/Pricing/Pricing';
 import Footer from 'components/Footer/Footer';
+import Example from 'components/Geographies/Geaographies';
 
 const Home = () => {
   return (
@@ -16,7 +17,7 @@ const Home = () => {
       <Services />
       <Expertise />
       <Questions />
-      <Geographies />
+      <Example />
       <Pricing />
       <Footer />
     </>
