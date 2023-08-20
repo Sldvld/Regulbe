7c97396110922e17bf74403dbaaf8baf0f6a56e6
Sldvlld <sladvlad@gmail.com>
2023-08-20 12:29:50 +0300

    add video

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,30 +2,38 @@ import backgroundImg from '../../assets/bckg.jpg';
 import sprite from '../../assets/sprite.svg';
 import css from './HeroSection.module.css';
 
+import video from '../../assets/video.mp4';
+
 const HeroSection = () => {
   return (
-    <div
-      className={css.bckg}
-      style={{ backgroundImage: `url(${backgroundImg})` }}
-      role="img"
-      aria-label="Background Image"
-    >
-      <div className={css.container}>
-        <div className={css.heroContainer}>
-          <h1 className={css.heroText}>
-            <span className={css.logoDecoration}>Regulbe </span> - regulatory
-            compliance services for cloud communications
-          </h1>
-
-          <button type="button" className={css.contactUsButton} to="/contactus">
-            <span className={css.buttonText}>Contact us</span>
-            <svg className={css.arrowSvg}>
-              <use href={sprite + '#icon-arrow'} />
-            </svg>
-          </button>
-        </div>
-      </div>
+    <div className={css.videoContainer}>
+      <video className={css.videoElement} autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
+    // <div
+    //   className={css.bckg}
+    //   style={{ backgroundImage: `url(${backgroundImg})` }}
+    //   role="img"
+    //   aria-label="Background Image"
+    // >
+    //   <div className={css.container}>
+    //     <div className={css.heroContainer}>
+    //       <h1 className={css.heroText}>
+    //         <span className={css.logoDecoration}>Regulbe </span> - regulatory
+    //         compliance services for cloud communications
+    //       </h1>
+
+    //       <button type="button" className={css.contactUsButton} to="/contactus">
+    //         <span className={css.buttonText}>Contact us</span>
+    //         <svg className={css.arrowSvg}>
+    //           <use href={sprite + '#icon-arrow'} />
+    //         </svg>
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
